ca5469ff101a043e157b7144b9b3accc84b3cf47
OsirisFrik <osiris@writecode.studio>
2020-03-12 13:07:46 -0700

    add test

---
+++ general.test.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+test('ENV', () => {
+  let errs = [];
+  let envs = [
+    'SESSION_SECRET',
+    'GH_CLIENT',
+    'GH_SECRET'
+  ]
+
+  for (let i = 0; i < envs.length; i++) {
+    if (typeof process.env[envs[i]] === 'undefined') {
+      errs.push(`ENV ${envs[i]} DON'T FOUND`);
+    }
+  }
+
+  return errs.length > 0 ? Promise.reject(errs) : Promise.resolve();
+});
